213f358069054ad3969142b0b8bf03e6bac35103
Muhammet Bulut <83714453+bunshiin@users.noreply.github.com>
2023-08-04 17:30:20 +0300

    arrow-function.js

---
+++ react-course-js-01/arrow-function.js
@@ -0,0 +1,24 @@
+//Arrow function (ok işareti fonksiyonu), JavaScript programlama dilinde fonksiyonları daha kısa ve sade bir şekilde tanımlamak için kullanılan bir ifadedir. 
+
+//Arrow function'lar, özellikle kısa ve basit fonksiyonlar için tercih edilir ve genellikle ES6 (ECMAScript 2015) ile birlikte tanıtılmıştır.
+
+//Arrow function'ların temel avantajı, fonksiyonları daha az yazıyla tanımlayabilmeleridir. 
+
+//Aynı zamanda this bağlamını otomatik olarak alır, bu da bazı durumlarda işleri daha basit hale getirebilir.
+
+//Arrow function'ın genel sözdizimi şu şekildedir:
+
+const functionName = (param1, param2, ...) => {
+    // Fonksiyon gövdesi
+    // İşlemler
+    return something;
+};
+
+
+//2 Sayıyı toplayak bir Arrow Function
+
+const functionName = (a, b) => {
+    return a + b;
+};
+
+console.log(functionName(5, 3)); // 8
