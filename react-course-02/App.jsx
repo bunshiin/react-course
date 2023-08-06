6cb79835cb31a8bd7e35610d88b7e2b8ebe82014
Muhammet Bulut <83714453+bunshiin@users.noreply.github.com>
2023-08-06 18:41:48 +0300

    Update App.jsx

--- react-course-02/App.jsx
+++ react-course-02/App.jsx
@@ -1,1 +1,41 @@
+import logo from './logo.svg';
+import './App.css';
 
+
+//FirstCompenent isminde bir yapı oluşturuyoruz.
+function FirstCompenent() {
+  return(
+    <div>
+      <span>FirstCompenent</span>
+    </div>
+    
+  )
+}
+
+
+function App() {
+  
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Muhammet Bulut
+        </p>
+        //Oluşturduğumuz bu yapıyı burada çağırarak "FirstCompenent" yazısını yazdırmamıza yaradı.
+        <FirstCompenent />
+
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          React Kursumuz
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
